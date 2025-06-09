f2e6387f4df0eb35075ff54662dbf06a0199d817
santuparsi <santuparsi@gmail.com>
2025-06-09 15:57:36 +0530

    commit6_9

---
+++ Week-3/demo.cs
@@ -0,0 +1,10 @@
+using System;
+class Demo
+{
+    static void Main(string[] args)
+    {
+        // This is a simple C# program that prints "Hello, World!" to the console.
+        System.Console.WriteLine("Hello, World!");
+        Console.ReadKey();
+    }
+}
