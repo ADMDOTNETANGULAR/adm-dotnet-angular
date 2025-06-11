d780c188123d1e8171a4d0b4b5969824b3ce84a9
santuparsi <santuparsi@gmail.com>
2025-06-11 11:12:50 +0530

    commit6_11

---
+++ Week-3/HandsOn/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+            int num = null;// This will cause a compile-time error because 'null' cannot be assigned to an int
+            int ? nullableNum = null; // This is valid because nullableNum is of type int? (nullable int)
+
+        }
+
+    }
+}
