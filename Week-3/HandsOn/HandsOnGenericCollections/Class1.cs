93d1fd1531b445a7638d88a2f54ccf2da17f2ab5
santuparsi <santuparsi@gmail.com>
2025-06-17 12:38:36 +0530

    commit6_17

---
+++ Week-3/HandsOn/HandsOnGenericCollections/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandsOnGenericCollections
+{
+    internal class Class1
+    {
+        public List<int> Ids=new List<int>();
+        static void Main()
+        {
+            Class1 obj = new Class1();
+            obj.Ids.Add(10); obj.Ids.Add(20);
+
+        }
+    }
+}
