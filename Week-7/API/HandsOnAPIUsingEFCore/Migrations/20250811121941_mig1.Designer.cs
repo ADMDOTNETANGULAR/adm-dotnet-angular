ea96ae65457ae1cbc61a3bec9268d40713b8c702
santuparsi <santuparsi@gmail.com>
2025-08-13 12:13:32 +0530

    commit8_12

--- Week-7/API/HandsOnAPIUsingEFCore/Migrations/20250811121941_mig1.Designer.cs
+++ Week-7/API/HandsOnAPIUsingEFCore/Migrations/20250811121941_mig1.Designer.cs
@@ -3,6 +3,7 @@ using HandsOnAPIUsingEFCore.DataContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HandsOnAPIUsingEFCore.Migrations
 {
     [DbContext(typeof(FlightDBContext))]
-    partial class FlightDBContextModelSnapshot : ModelSnapshot
+    [Migration("20250811121941_mig1")]
+    partial class mig1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
